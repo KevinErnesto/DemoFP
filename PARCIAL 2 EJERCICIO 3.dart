1c456c80c2e6e2affa3d63bd541312e1e9b333f1
Kevin Ernesto Santana Lopez <111769170+KevinErnesto@users.noreply.github.com>
2022-10-26 22:00:02 -0500

    Create PARCIAL 2 EJERCICIO 3.dart

---
+++ PARCIAL 2 EJERCICIO 3.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+main() {
+
+  division
+  print(10/3) trumcate();
+  print(10~/3);
+
+  print(cos(45* pi / 100));
+  print(sin(45* pi / 100));
+  print(sqrt(9));
+  print(pow(2, 3));
+  print(max(6, 10));
+  print(min(6, 10));
+}
