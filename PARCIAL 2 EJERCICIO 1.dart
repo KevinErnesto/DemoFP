30b1d38018a6304e0b6a59f283fbba8a2a245cbf
Kevin Ernesto Santana Lopez <111769170+KevinErnesto@users.noreply.github.com>
2022-10-26 21:57:38 -0500

    Create PARCIAL 2 EJERCICIO 1.dart

---
+++ PARCIAL 2 EJERCICIO 1.dart
@@ -0,0 +1,19 @@
+void main()  {
+    num a = 3;
+    print(a.isEven);
+
+
+    var infint= 5
+    var infdouble= 9.81;
+    print("${infint.runtimeType}");
+    print("${infdouble.runtimeType}");
+
+    num infnum;
+    infnum= 3.6;
+    print("${infnum.runtimeType}");
+    intnum= 5;
+    print("${infnum.runtimeType}");
+
+    print(5.isEven);
+    
+    }
