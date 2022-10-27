dd1bbfd15228403a4de8639380dc528701685ca9
Kevin Ernesto Santana Lopez <111769170+KevinErnesto@users.noreply.github.com>
2022-10-26 21:58:41 -0500

    Create PARCIAL 2 EJERCICIO 2.dart

---
+++ PARCIAL 2 EJERCICIO 2.dart
@@ -0,0 +1,18 @@
+void main(List<string> args){
+  int a = -3000;
+ double b =9.5;
+
+  print(a.isNegative);
+  
+  print(b.floor());
+  print(b.cell());
+
+  print(b.round());
+  
+  print(b.truncate());
+
+  var c = "Hola"
+  
+  print(b.isNaN);
+
+}
