ec723c2b6758a6069faa9534da91c3b3c7a360a7
Kevin Ernesto Santana Lopez <111769170+KevinErnesto@users.noreply.github.com>
2022-10-26 22:02:05 -0500

    Create PARCIAL 2 EJERCICIO 5.dart

---
+++ PARCIAL 2 EJERCICIO 5.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int a= 5;
+  double b= 3.5;
+
+  print(a.toDouble());
+  print(b.toInt());
+  print("El valor es: $a");
+  print("El valor es: "+ a.toSting());
+  print(a+b);
+  print(a/b);
+
+}
